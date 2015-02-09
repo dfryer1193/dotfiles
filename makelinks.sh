ae618b152cad9d6d4d4e1adb9776c43ca109e3e8
David Fryer <dfryer1193@gmail.com>
2015-02-09 16:10:15 -0500

    Added support for directories

--- makelinks.sh
+++ makelinks.sh
@@ -5,10 +5,12 @@ create_msg() { echo "[ CREATE ] $@"; }
 exists_msg() { echo "[ EXISTS ] $@"; }
 
 makelink() {
-  fname=$1
-
-  if [[ ! -e $HOME/$fname ]]; then
-    ln -s $HOME/dotfiles/$fname $HOME/$fname
+  fname=$HOME/$1
+  if [[ ! -e $fname ]]; then
+    if [[ ! -e ${fname%/*} ]]; then
+      mkdir -p ${fname%/*}
+    fi
+    ln -s $HOME/dotfiles/${fname##*/} $fname
     create_msg "$fname"
   else
     exists_msg "$fname"
