c0f930d6d4d4cd90dfda85cb0f9cdd3a29d62f19
David Fryer <dfryer1193@gmail.com>
2016-02-18 08:47:56 -0500

    I like changing colors.
    This change requires i3lock-color (arch: aur/i3lock-color-git)

--- .bin/screenlock.sh
+++ .bin/screenlock.sh
@@ -2,4 +2,18 @@
 
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x7 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+rm -f /tmp/screenshot.png
+
+i3lock -i /tmp/screenshotblur.png \
+  --insidevercolor=dddddd55 \
+  --insidewrongcolor=e84f4f55 \
+  --insidecolor=66666655 \
+  --ringvercolor=ccccccff \
+  --ringwrongcolor=d23d3dff \
+  --ringcolor=222222ff \
+  --textcolor=00000000 \
+  --keyhlcolor=dddddd22 \
+  --bshlcolor=e84f4f22
+
+# cleanup
+rm -f /tmp/screenshotblur.png
