1e6bcbfdf5ffd788d66175af75919a56f6d2cd8b
David Fryer <dfryer1193@gmail.com>
2016-03-19 15:24:12 -0400

    Handle both possible i3lock binaries

--- .bin/screenlock.sh
+++ .bin/screenlock.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [[ ! $(type i3lock &>/dev/null) ]]; then
+  locker=i3lock
+else
+  locker=i3lock-color
+fi
+
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x7 /tmp/screenshotblur.png
 rm -f /tmp/screenshot.png
 
-i3lock-color -i /tmp/screenshotblur.png \
+$locker -i /tmp/screenshotblur.png \
   --insidevercolor=dddddd55 \
   --insidewrongcolor=e84f4f55 \
   --insidecolor=66666655 \
