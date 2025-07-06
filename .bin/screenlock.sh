6bc014cb2ce6df76048ed26d8abfccc408980ab9
David Fryer <dfryer1193@gmail.com>
2025-07-05 21:26:35 -0400

    Update screenlock to use aur/swaylock-effects

--- .bin/screenlock.sh
+++ .bin/screenlock.sh
@@ -4,23 +4,28 @@ DUNST_STATE=$(dunstctl is-paused)
 
 dunstctl set-paused true
 
-i3lock -B 10 \
-   -k \
-   -n \
-  --insidever-color=dddddd55 \
-  --insidewrong-color=e84f4f55 \
-  --inside-color=66666655 \
-  --ringver-color=ccccccff \
-  --ringwrong-color=d23d3dff \
-  --ring-color=222222ff \
-  --line-uses-inside \
-  --keyhl-color=dddddd22 \
-  --bshl-color=e84f4f22 \
-  --time-str="%H:%M:%S" \
-  --time-pos="x+w-(1.5*r):y+h-(0.5*r)" \
-  --time-color=ccccccff \
-  --time-font=monospace \
-  --time-size=25 \
-  --date-str=""
+swaylock \
+   --daemonize \
+   --screenshots \
+   --effect-blur 10x5 \
+   --clock \
+   --indicator \
+   --font "monospace" \
+   --timestr "%H.%M.%S" \
+   --datestr "%Y.%m.%d" \
+   --indicator-radius 100 \
+   --indicator-thickness 5 \
+   --ring-color 16161633 \
+   --ring-caps-lock-color ffe863ff \
+   --text-color cccccc \
+   --text-clear-color cccccc \
+   --text-ver-color cccccc \
+   --text-wrong-color cccccc \
+   --key-hl-color b7ce42 \
+   --ring-ver-color 66aabb \
+   --inside-color 161616 \
+   --inside-ver-color 161616 \
+   --inside-wrong-color 161616 \
+   --inside-clear-color 161616
 
 dunstctl set-paused $DUNST_STATE
